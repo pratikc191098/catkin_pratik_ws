55f75b75cf23cb537f837bc03d57e8d13af084dd
pratik chaudhari <pratikc191098@gmail.com>
2021-10-09 14:07:04 +0530

    turtlebot 3 installed and compiled successfully in catkin pratik ws

---
+++ build/turtlebot3/turtlebot3_example/CMakeFiles/turtlebot3_example_generate_messages_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/turtlebot3_example_generate_messages_cpp"
+  "/home/pnt/catkin_pratik_ws/devel/include/turtlebot3_example/Turtlebot3Action.h"
+  "/home/pnt/catkin_pratik_ws/devel/include/turtlebot3_example/Turtlebot3Goal.h"
+  "/home/pnt/catkin_pratik_ws/devel/include/turtlebot3_example/Turtlebot3Result.h"
+  "/home/pnt/catkin_pratik_ws/devel/include/turtlebot3_example/Turtlebot3ActionGoal.h"
+  "/home/pnt/catkin_pratik_ws/devel/include/turtlebot3_example/Turtlebot3ActionFeedback.h"
+  "/home/pnt/catkin_pratik_ws/devel/include/turtlebot3_example/Turtlebot3Feedback.h"
+  "/home/pnt/catkin_pratik_ws/devel/include/turtlebot3_example/Turtlebot3ActionResult.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/turtlebot3_example_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/turtlebot3/turtlebot3_navigation/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pnt/catkin_pratik_ws/src/turtlebot3/turtlebot3_navigation
+# Build directory: /home/pnt/catkin_pratik_ws/build/turtlebot3/turtlebot3_navigation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
